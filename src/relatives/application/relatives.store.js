42d87bb9032b2243131b7553cd18567a60cf1569
MarceBq <marcebq2005@gmail.com>
2025-10-08 04:45:20 -0500

    feat(relatives): add store to relatives bounded context

---
+++ src/relatives/application/relatives.store.js
@@ -0,0 +1,42 @@
+import {RelativesApi} from "../infrastructure/relatives-api.js";
+import {defineStore} from "pinia";
+import {computed, ref} from "vue";
+import {RelativeAssembler} from "../infrastructure/relative.assembler.js";
+
+const USER_EXAMPLE_DATA_1 = {
+    "id": 1,
+    "email": "valeria@gmail.com",
+    "password": "valeria123",
+    "role": "relative",
+    "entityId": 1
+}
+
+const USER_EXAMPLE_DATA_2 = {
+    "id": 1,
+    "email": "valeria@gmail.com",
+    "password": "valeria123",
+    "role": "relative",
+    "entityId": 2
+}
+
+const relativesApi = new RelativesApi()
+
+export const useRelativesStore = defineStore('relatives', () => {
+    // State
+    const relative = ref(null);
+    const errors = ref([]);
+
+    function fetchRelativeData(){
+        relativesApi.getRelativeById(USER_EXAMPLE_DATA_1.entityId)
+            .then(response => {
+                relative.value = RelativeAssembler.toEntityFromResource(response.data);
+                console.log("Relative Entity:", relative.value);
+            })
+    }
+
+    return {
+        relative,
+        errors,
+        fetchRelativeData,
+    }
+})
