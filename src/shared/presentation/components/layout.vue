1bd6134d7df2093f8634c1aa617cc148a4d1591e
GabrielRiveraOfficial <gabriverlay@gmail.com>
2025-11-12 03:15:27 -0500

    fix: fixed style issues with the login page

--- src/shared/presentation/components/layout.vue
+++ src/shared/presentation/components/layout.vue
@@ -1,9 +1,16 @@
 <script setup>
 import AppHeader from "./app-header.vue";
+import { useRoute } from "vue-router";
+import { computed } from "vue";
+
+const route = useRoute();
+
+// Hide header on login page
+const showHeader = computed(() => route.name !== 'login');
 </script>
 
 <template>
-  <app-header></app-header>
+  <app-header v-if="showHeader"></app-header>
   <router-view/>
 </template>
 
