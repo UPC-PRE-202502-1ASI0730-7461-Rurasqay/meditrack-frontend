3a4db49df42dfce3cf9332213915b826a95e8ce5
GabrielRiveraOfficial <gabriverlay@gmail.com>
2025-12-01 02:27:16 -0500

    fix: fixed reference to app.vue

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
-import App from './app.vue'
-import i18n from "./i18n.js";
+import App from './App.vue'
+import i18n from "./i18n.js"; 
 import 'primeflex/primeflex.css';
 import 'primeicons/primeicons.css';
 import PrimeVue from 'primevue/config';
